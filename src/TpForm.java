ad6a32efda9f353b3959cc4a7419bbd6d23a23ed
Xiraj <akbarsirajuddinh@gmail.com>
2022-03-04 01:50:12 +0700

    Initial Commit

--- src/TpForm.java
+++ src/TpForm.java
@@ -1,28 +1,55 @@
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class TpForm extends JFrame {
-    private JTextField userField;
-    private JPanel panel1;
-    private JButton submitButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-    public TpForm() {
-        submitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(submitButton, userField.getText());
+public class TpForm extends JFrame
+{
+    JPanel panel = new JPanel();
+    JLabel result = new JLabel();
+    JTextField textField = new JTextField(30);
+    JButton submit = new JButton("Enter");
+
+    public TpForm()
+    {
+        setTitle("Tutorial");
+        setVisible(true);
+        setSize(400, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        panel.add(textField);
+
+
+        textField.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String input = textField.getText();
+                result.setText(input);
             }
         });
+
+        panel.add(submit);
+        submit.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String input = textField.getText();
+                result.setText("Name: "+input);
+            }
+        });
+
+        panel.add(result);
+        add(panel);
+
     }
 
-    public static void main(String[]args) {
-        TpForm frame = new TpForm();
-        frame.setContentPane(frame.panel1);
-        frame.setTitle("Tp Data");
-        frame.setBounds(600, 200, 200, 200);
-        frame.setSize(300, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    public static void main(String[] args)
+    {
+        TpForm t = new TpForm();
     }
 }
